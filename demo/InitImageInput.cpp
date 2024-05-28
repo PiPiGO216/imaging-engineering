7782663bc1a63889a92a4f8f6defbec419e0a9d8
PiPiGO <liuch226@qq.com>
2024-05-29 00:28:49 +0800

    Add one image and code demo

---
+++ demo/InitImageInput.cpp
@@ -0,0 +1,15 @@
+#include <opencv2/opencv.hpp>
+#include <string>
+
+int main()
+{
+    std::string imgName = "../ImageSource/IMG20230726.jpg";
+    //std::string imgName = "..\\ImageSource\\IMG20230726.jpg";
+    cv::Mat imgInput = cv::imread(imgName, -1);
+    if (imgInput.empty())
+    {
+        printf("Cannot Read Current Image");
+        return 0;
+    }
+    return 1;
+}
